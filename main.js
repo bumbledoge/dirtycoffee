525ea7b5f898f28aae6c7b3ce7c3eadd34520cdf
Cristi Gujan <dariusppopp@gmail.com>
2023-12-04 09:16:17 +0200

    well it's moving i guess

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
+const loader = new GLTFLoader();
 const axesHelper = new THREE.AxesHelper(30);
 const marimi = {
   height: window.innerHeight,
@@ -8,35 +10,51 @@ const marimi = {
 const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   marimi.width / marimi.height,
   0.1,
   100
 );
-camera.position.z = 5;
+camera.position.z = 8;
 scene.add(camera);
-scene.add(axesHelper);
+// scene.add(axesHelper);
 
-const textbox = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshLambertMaterial({ color: 0x008dd5 })
-);
-scene.add(textbox);
+//objects
+let bijou = undefined;
+loader.load("./public/bijou.glb", (gltf) => {
+  console.log("done");
+  bijou = gltf.scene;
+  gltf.scene.position.y = -1;
+  gltf.scene.position.x = -3;
+  gltf.scene.position.z = -1;
+  gltf.scene.rotation.x = Math.PI / 6;
+  scene.add(gltf.scene);
+});
 
 //lights
-var light = new THREE.PointLight("white", 20);
-var light1 = new THREE.PointLight("white", 20);
-light.position.set(3, 2, 2);
-light1.position.set(-3, 2, 2);
+var light = new THREE.PointLight("white", 300);
+var light1 = new THREE.PointLight("#433423", 20);
+light.position.set(-2, 4, 5);
+light1.position.set(3, 0, -2);
 scene.add(light, light1);
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
 renderer.setSize(marimi.width, marimi.height);
 renderer.render(scene, camera);
 
-console.log(textbox, camera);
+//animations
+const clock = new THREE.Clock();
+let pastTime;
+window.addEventListener("mousemove", (event) => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = pastTime - elapsedTime;
+  bijou && (bijou.position.x += event.pageX / marimi.width / 1000);
+  bijou && (bijou.position.y += -event.pageY / marimi.height / 1000);
+  pastTime = elapsedTime;
+});
 
 const tick = () => {
+  bijou && (bijou.rotation.y += 0.005);
   renderer.render(scene, camera);
   requestAnimationFrame(tick);
 };
